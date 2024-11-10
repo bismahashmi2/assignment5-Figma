365fb62c40b391083c98a93943df91e1b6dc76e7
Hashmi Hashmi <119hashmihashmi@gmail.com>
2024-11-10 15:06:17 +0500

    Thursday morning class assignment5

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className= 'w-[1420px] h-[134px] bg-[#A29875]'>
+      <h1 className=' pl-[36px] text-[#FFFFFF] font-[Algerian] text-[75px]'> MANZZARI</h1>
+    </div>
+    
+  )
+}
+
+export default Header
